1bf2ae881e8bc168457f7514836bc5f851d8e221
jux89 <j_u_x@hotmail.com>
2018-06-16 10:17:35 -0300

    ajuste-insercao-de-dados

--- Script/Inserção-de-Dados.sql
+++ Script/Inserção-de-Dados.sql
@@ -1,8 +1,3 @@
-INSERT INTO sgg.pessoa (nome, telefone)
-	SELECT NOME, TELEFONE FROM bdii.pessoa;
-
-INSERT INTO sgg.pessoa (endereco)
-	SELECT CONCAT_WS (', ', LOGRADOURO, NUMERO) AS ENDERECO FROM bdii.endereco;
 
 INSERT INTO sgg.pessoa (nome, telefone, endereco)
 	SELECT d.NOME, d.TELEFONE, CONCAT_WS (', ', e.LOGRADOURO, e.NUMERO) FROM bdii.pessoa d
