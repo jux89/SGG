25a22d558f2fe95b57547b5b1c9cc663d5f16a01
jux89 <j_u_x@hotmail.com>
2018-06-17 18:25:26 -0300

    atualizacao-funcao

--- Scripts/Funções/Função InLine Table.sql
+++ Scripts/Funções/Função InLine Table.sql
@@ -1,13 +1,13 @@
 
-/*Função Inline table-valued => Solução: Mostrar a quantidade de bezerros que nasceram após certo período*/
+/*Função Inline table-valued => Solução: Mostrar quais bezerros nasceram após certo período*/
 DELIMITER //
 CREATE FUNCTION fcBezerrosApos (dataApos date)
 	RETURNS date
-	BEGIN 
+	 
 		return (SELECT *
         FROM sgg.reproducao 
         WHERE parto >= dataApos);
-    END
+    
 // DELIMITER ; 
 
 /* Chama a função, passa como parâmetro a data desejada*/
