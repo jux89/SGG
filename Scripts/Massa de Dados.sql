9f555079880e27085126eeaa4bec4a023c73dacc
MatheusBuenevides <matheusgomesbuenevides@gmail.com>
2018-06-19 11:45:29 -0300

    Novas inserções de dados

--- Scripts/Massa de Dados.sql
+++ Scripts/Massa de Dados.sql
@@ -12,3 +12,9 @@ INSERT INTO cabanha (endereco, id_pessoa) SELECT endereco,id_pessoa FROM pessoa;
 
 -- Inserção de dados na Tabela Galpão
 INSERT INTO galpao (nome, capacidade, id_cabanha) SELECT CONCAT('Cabanha ', id_cabanha) AS nome, (id_cabanha + 10), id_cabanha FROM cabanha;
+
+-- Inserção de dados na Tabela Raça
+INSERT INTO raca (nome) VALUES ("Sims"),("Morgan"),("English"),("Herrera"),("Henson"),("Stein"),("Miranda"),("Barlow"),("Fernandez"),("Mckenzie"),("Cox"),("Reynolds"),("Delacruz"),("Andrews"),("Mack"),("Leblanc"),("David"),("Garrison"),("Rasmussen"),("Morrow"),("Sanford"),("Deleon"),("Buckner"),("Jackson"),("Lewis"),("Carroll"),("Bauer"),("Sharpe"),("David"),("Banks"),("Hudson"),("Mckinney"),("Morton"),("Marks"),("Gonzalez"),("Noble"),("Allen"),("Greer"),("Brown"),("Dyer"),("Nicholson"),("Fox"),("Reed"),("Melendez"),("Langley"),("Salazar"),("Robinson"),("Hensley"),("Gaines"),("Puckett"),("Bryant"),("Wolfe"),("Maddox"),("Brock"),("Dickerson"),("Walls"),("Knox"),("Andrews"),("Malone"),("Barnes"),("Cantu"),("Lowery"),("Madden"),("West"),("Branch"),("Dillard"),("Castillo"),("Hicks"),("Warner"),("Mueller"),("Sanchez"),("Dawson"),("Chapman"),("Bryan"),("Kelly"),("Vasquez"),("Roberson"),("Richards"),("Frank"),("Fox"),("Meyers"),("Brennan"),("Guy"),("Hancock"),("Strong"),("Ward"),("Calhoun"),("Morse"),("Rivas"),("Bauer"),("Andrews"),("Francis"),("Gates"),("Poole"),("Lamb"),("Cooper"),("Holder"),("Bell"),("Weaver"),("Levine");
+
+-- Inserção de dados na Tabela Animal
+insert into animal (peso,id_raca, id_galpao) select floor(200+(rand()*(1-200))) , floor(10+(rand()*(1-10))) , floor(200+(rand()*(1-200)))from pessoa
