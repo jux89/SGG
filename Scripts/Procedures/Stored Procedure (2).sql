2da5ef669d31aca39eead2044ec34e6822f1a5db
jux89 <j_u_x@hotmail.com>
2018-06-18 22:50:25 -0300

    ajuste-nome-sp

--- Scripts/Procedures/Stored Procedure (2).sql
+++ Scripts/Procedures/Stored Procedure (2).sql
@@ -1,6 +1,6 @@
 -- SEGUNDA PROCEDURE
 DELIMITER ;
-CREATE PROCEDURE tabela_animal (IN id_raca INT, IN id_galpao INT, OUT id_animal INT)
+CREATE PROCEDURE insere_vaca (IN id_raca INT, IN id_galpao INT, OUT id_animal INT)
 BEGIN
 	INSERT INTO animal(id_raca,id_galpao) VALUES (@id_raca, @id_galpao);
 	SELECT LAST_INSERT_ID();
